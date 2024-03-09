ecf060c914e90aba374b0db7c7fe714925e282df
sudoC7 <slowjacob7@gmail.com>
2024-03-09 21:30:56 +0100

    Add a new function and the buttons

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -1,20 +1,40 @@
 
 let firstCard = 11
-let secondCard = 10
-let sum = firstCard + secondCard + 4
+let secondCard = 10 
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true 
- let message = ""
+let message = ""
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-El")
+let cardsEL = document.getElementById("cards-El");
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card ?"
-} else if (sum === 21) {
-    message = "You've got BlackJack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! "
-    isAlive = false 
+function startGame() {
+   
+    cardsEL.textContent = "Cards: " + firstCard + " " + secondCard 
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card ?"
+    } else if (sum === 21) {
+        message = "You've got BlackJack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false 
+    }
+    messageEl.textContent = message
+    
 }
 
 
-console.log(message)
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+
+    //1. Create a card variable, and hard code its value to a number (2-11)
+
+    //2. Add the new card to the sum variable
+
+    //3. Call startGame()
+
+
+}
