9a199262439f5bbc61ebf50e485d5cb35b29205d
sudoC7 <slowjacob7@gmail.com>
2024-03-10 20:11:09 +0100

    adding of some functions

--- Progjs1/gamejs1/scripts/script.js
+++ Progjs1/gamejs1/scripts/script.js
@@ -1,4 +1,12 @@
 
+        let globalScore = 0
+        let a = 0
+
+        function afficherEmail(nom, email, score) {
+            let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+            location.href = mailto
+        }
+        
         //Fonction affiche les propositions du tableau
         function afficherProposition(leMotDuListe){
             let motAfficher = document.querySelector('.zoneProposition')           
@@ -30,6 +38,10 @@
                     score++
                 }
                 i++
+
+                // Attribution au score global
+                globalScore = score 
+                a = i
                 afficheResultat(score, i)                   
                 lInputButton1.value = ''
                 afficherProposition(leTableau[i])
@@ -59,5 +71,28 @@
             
         }
 
-
+        // Fonction permet de faire apparaitre le formulaire de partage SCORE 
+        let partage = document.getElementById("button2")
+        partage.addEventListener('click', () => {
+            let popupBack = document.querySelector(".popupBackground").style.display="flex";
+        })
    
+
+
+        //Fonction qui empechera la page de s'actualiser lors de Submit 
+        
+        let form = document.querySelector('form')
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
+            console.log("LA page ne s'est pas actualisé !!!")
+
+            const nom = document.getElementById("nom").value
+            const email = document.getElementById("email").value
+
+            let leScore = globalScore + "/" + a
+            
+            afficherEmail(nom, email, leScore)
+            
+            let popupBack = document.querySelector(".popupBackground").style.display="none"
+        })
